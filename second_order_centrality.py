e7c8ed1b7773feec56393460a070c15fdbda5419
Nicolas Schreuder <schreuder.nicolas@gmail.com>
2016-11-15 09:52:51 +0100

    Update second_order_centrality.py

--- second_order_centrality.py
+++ second_order_centrality.py
@@ -31,13 +31,13 @@ def sigma_i(l,n):
 
 # the following algorithm allows me to
 def second_order_centrality(graphe,N):
-    card_vertices = len(graphe[0])
-    random_walk_pos = randint(0,card_vertices-1)
+    graph_order = graphe.shape[0]
+    random_walk_loc = randint(0,graph_order-1)
     #print((" Noeud initial"),random_walk_pos)
 
-    visited = [-1]*card_vertices
-    epsilon = [[]]*card_vertices
-    sigma = [[]]*card_vertices
+    visited = [-1]*graph_order
+    epsilon = [[]]*graph_order
+    sigma = [[]]*graph_order
     iter = 0
     cont = True
     while cont==True:
@@ -49,31 +49,31 @@ def second_order_centrality(graphe,N):
         else:
             iter += 1
             #print("----- nouvelle entrée dans la boucle")
-            surrounding=neighbor(graphe,random_walk_pos)
+            surrounding=neighbor(graphe,random_walk_loc)
             #print(("je suis situé en"),random_walk_pos)
             #print("")
             #print(("les voisins sont "),surrounding)
-            next_pos = surrounding[randint(0,len(surrounding)-1)]
-            d_j = len(neighbor(graphe,next_pos))
+            next_loc = surrounding[randint(0,len(surrounding)-1)]
+            d_j = len(neighbor(graphe,next_loc))
             d_i = len(surrounding)
             p = uniform(0,1)
             if p<=d_i/d_j:
                 #print(("deplacement vers",next_pos))
-                random_walk_pos=next_pos
+                random_walk_loc=next_loc
             else:
                 pass
 
-            if visited[random_walk_pos]==-1:
+            if visited[random_walk_loc]==-1:
                 #print("je reste à l'ancienne position")
                 #print(("detection d'un point jamais encore visite, première visite en t="),iter)
-                visited[random_walk_pos]=iter
+                visited[random_walk_loc]=iter
             else:
                 # how many times did we visit the vertice n°i
-                epsilon[random_walk_pos]=epsilon[random_walk_pos]+[iter-visited[random_walk_pos]]
+                epsilon[random_walk_loc] = epsilon[random_walk_loc] + [iter-visited[random_walk_loc]]
                 #print("ce n'est pas ma première visite ici")
                 #print(("voici les temps de retour calculés"),epsilon[random_walk_pos])
-                if len(epsilon[random_walk_pos])>3:
-                    sigma[random_walk_pos]=sigma[random_walk_pos]+[sigma_i(epsilon[random_walk_pos],len(epsilon[random_walk_pos]))]
+                if len(epsilon[random_walk_loc])>3:
+                    sigma[random_walk_loc] = sigma[random_walk_loc] + [sigma_i(epsilon[random_walk_loc],len(epsilon[random_walk_loc]))]
 
     return epsilon,sigma
 
