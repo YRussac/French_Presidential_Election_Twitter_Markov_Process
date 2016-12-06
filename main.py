b6e462368542a1e8b376916f29fc8f1729401370
NICOLAS SCHREUDER <schreuder.nicolas@gmail.com>
2016-12-06 07:36:35 +0100

    Removed unnecessary file (creation_graph.py) Split second_order_centrality.py in two files : main.py and functions.py Added function to compute theoretical values and transition matrix

--- main.py
+++ main.py
@@ -6,14 +6,18 @@ from functions import *
 G = nx.karate_club_graph()
 B = nx.adjacency_matrix(G).toarray()
 
+
+tm = transition_mat(B)
+theoretical_values = theoretical_second_order_centrality(tm)
+
 # For reproducibility
 seed(42)
 
 plot = True
-n = 500
+n = 2000
 
 # We compute the first and second order centrality for the graph we loaded
-eps, m, s = second_order_centrality(B, n)
+eps, m, s = graph_centrality(B, n)
 
 colors_m = [l[-1] for l in m]
 colors_s = [l[-1] for l in s]
@@ -25,19 +29,16 @@ nx.draw_networkx(G, node_color=colors_s, alpha=0.5, cmap=plt.get_cmap('bwr'))
 if plot:
     x = [i for i in range(n-2)]
 
-    plt.subplots(nrows=5, ncols=2, sharex=True, figsize=(6, 6))
+    plt.subplots(nrows=5, ncols=3, sharex=True, figsize=(6, 6))
 
     print('Plotting')
 
-    for i in range(10):
-        ax = plt.subplot(5, 2, i + 1)
+    for i in range(15):
+        ax = plt.subplot(5, 3, i + 1)
         ax.yaxis.tick_right()
-        ax.title.set_text('Centralité de second ordre du noeud {}'.format(i))
+        ax.title.set_text('Node {} 2nd order centrality'.format(i))
         ax.plot(x, s[i][:n - 2])
+        ax.axhline(y=theoretical_values[i], c="red", linewidth=3, zorder=0)
 
     plt.tight_layout()
     plt.show()
-
-
-
-
