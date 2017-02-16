7c62d0e648d797db71c64d71a07842c2b0ec2128
NICOLAS SCHREUDER <schreuder.nicolas@gmail.com>
2017-02-16 15:23:14 +0100

    Operational bot

--- stream_and_scrap.py
+++ stream_and_scrap.py
@@ -4,11 +4,7 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 import tweepy
-import pprint
-import twitter
 import time
-import io
-import ast
 
 CONSUMER_KEY = os.environ["CONSUMER_KEY"]
 CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
@@ -18,113 +14,73 @@ OAUTH_TOKEN_SECRET = os.environ["OAUTH_TOKEN_SECRET"]
 auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 api = tweepy.API(auth)
-start_time = time.time() #grabs the system time
-users_observed = {}
 
 
-def information_user(id,tweets_number,dico):
-    """
-    :param id: c'est l'id de l'utilisateur twitter que l'on considère
-    :param tweets_number: nombre de derniers tweet sur lesquels on récupère les infos
-    :param dico: dictionnaire qui devra contenir l'ensemble des informations pour les utilisateurs observés
-    :return: actualise le dictionnaire avec les nouvelles valeurs
-    """
-    user = api.get_user(id)
-    dico[id] = {}
-    print(user.screen_name)
-    dico[id]["user.screen_name"] = user.screen_name
-    print(user.followers_count)
-    dico[id]["user.followers_count"] = user.followers_count
-    print(user.friends_count)
-    dico[id]["user.friends_count"] = user.friends_count
-    statuses = api.user_timeline(id,count=tweets_number)
-    dico[id]["statuses"] = {}
-    dico[id]["statuses"]["id_tweet"] = []
-    dico[id]["statuses"]["Text"] = []
-    dico[id]["statuses"]["Geo"] = []
-    dico[id]["statuses"]["hashtags"] = []
-    dico[id]["statuses"]["user_mentions"] = []
+def user_information(user_id, number_of_tweets=200):
+    user = api.get_user(user_id)
+
+    d_json = {"screen_name": user.screen_name,  "followers_count": user.followers_count,
+              "friends_count": user.friends_count, "tweets": []}
+
+    statuses = api.user_timeline(user_id, count=number_of_tweets)
+
     for tweet in statuses:
-        print("Observation d'un nouveau tweet...")
-        print("|||")
-        print("id du tweet:")
-        print(tweet.id)
-        dico[id]["statuses"]["id_tweet"] += [tweet.id]
-        print("Text du tweet observé:")
-        print(tweet.text)
-        dico[id]["statuses"]["Text"] += [tweet.text]
-        print("géographie:")
-        print(tweet.geo)
-        dico[id]["statuses"]["Geo"] += [tweet.geo]
-        print("Mention du Tweet:")
-        print(tweet.entities["hashtags"])
-        print(tweet.entities["user_mentions"])
-        dico[id]["statuses"]["hashtags"] += [tweet.entities["hashtags"]]
-        dico[id]["statuses"]["user_mentions"] += [tweet.entities["user_mentions"]]
-    print("Fin affichage des informations utilisateurs et mentions")
-    print(dico[id])
-        # Rq: dans id pour user mentions c'est bien l'id du compte tweeter mentionné.
-        # la fonction information_user fonctionne par effet de bords!
+        d_tweet = {"tweet_id": tweet.id_str, "text": tweet.text, "geo": tweet.geo,
+                   "hashtags": tweet.entities["hashtags"], "user_mentions": tweet.entities["user_mentions"],
+                   "date": tweet.created_at}
+        d_json["tweets"].append(d_tweet)
+
+    print(d_json)
+    print("ℵ")
+    return True
+
 
 class StdOutListener(StreamListener):
 
-    def __init__(self,start_time,time_limit=60):
-        self.time = start_time
+    def __init__(self, time_limit=10000):
+        self.time = time.time()
         self.limit = time_limit
         self.tweet_data = []
 
-
     def on_data(self, data):
-        saveFile = io.open('raw_tweets.json', 'a', encoding='utf-8')
-        saveFile2 = io.open('info_utilisateurs.txt', 'a', encoding='utf-8')
-        while (time.time() - self.time) < self.limit:
-            #print(data)
-            print("Affichage de l'id d'utilisateur:")
-            decoded=json.loads(data)
-            print(decoded["user"]["id"])
-            temp_user_id=decoded["user"]["id"]
-            if temp_user_id not in users_observed:
-                print("Utilisateur non observé !")
-                # We had the information for this unobserved user
-                information_user(temp_user_id, 2, users_observed)
-            print("------")
-            print()
+        decoded = json.loads(data)
 
-        #print('@%s: %s' % (decoded['user']['screen_name'], decoded['text'].encode('ascii', 'ignore')))
-        #champs utiles à premiere vue: text, id_str, user[screen_name],user['id_str'], geo, statuses_count, friends_count,created_at,followers_count
-            try:
-                self.tweet_data.append(data)
+        user_id = decoded["user"]["id_str"]
 
-            except BaseException as e:
-                print('failed ondata,', str(e))
-                time.sleep(60)
-                pass
-            return True
-        saveFile = io.open('raw_tweets.json', 'w', encoding='utf-8')
-        saveFile.write(u'[\n')
-        saveFile.write(','.join(self.tweet_data))
-        saveFile.write(u'\n]')
-        saveFile.close()
-        print("le dictionnaire d'information ressemble à:")
-        print(users_observed)
-        saveFile2.write(str(users_observed))
-        saveFile2.close()
-        exit()
+        if user_id not in observed_users:
+            # New user
+            # We add the information for this unobserved user
+            too_many_requests = True
+            while too_many_requests:
+                try:
+                    user_information(user_id, 50)
+                    observed_users[user_id] = 1
+                    too_many_requests = False
+                except:
+                    time.sleep(10)
+        else:
+            observed_users[user_id] += 1
 
+        if (time.time() - self.time) > self.limit:
+            print(observed_users)
+            exit()
+
+        return True
 
     def on_error(self, status):
-        print(status)
-        print("j'affiche status")
-        print("------")
-        print("ZZZZZZZZZZZZZZZZZZZZZZZ")
+        print("Error !")
 
 if __name__ == '__main__':
-    #This handles Twitter authetification and the connection to Twitter Streaming API
-    l = StdOutListener(start_time,time_limit=10)
+
+    observed_users = {}
+
+    l = StdOutListener(time_limit=300)
     auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
     stream = Stream(auth, l)
 
-    #This line filter Twitter Streams to capture data by the #hashtags of the french candidates for the presidential election
+    # This line filter Twitter Streams to capture data by the hashtags of the french candidates for
+    # the presidential election
+
     stream.filter(track=["#Macron2017", "#Hamon2017", "#MLP2017", "#Fillon2017", "#JLM2017", "#NDA2017", "#Jadot2017"])
 
