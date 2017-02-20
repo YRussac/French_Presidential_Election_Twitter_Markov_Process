fa388c5e357c5433f54a57e1bb77035844ef56c4
lauratinsi <lauratinsi@yahoo.fr>
2017-02-20 19:07:07 +0100

    Update parsing.py

--- parsing.py
+++ parsing.py
@@ -20,3 +20,23 @@ users_count = dict_list[-1]
 #dict_list = [eval(j) for j in l]
 
 
+def links(users_list, n, users_id):
+    m = []
+    for i in range(0, len(users_list[n]['tweets'])):
+        for j in range(0, len(users_list[n]['tweets'][i]['user_mentions'])):
+            if users_list[n]['tweets'][i]['user_mentions'][j] in users_id:
+                #print("il a utilisé le hashtag")
+                if users_list[n]['tweets'][i]['user_mentions'][j] not in m:
+                    m.append(users_list[n]['tweets'][i]['user_mentions'][j])
+    return m
+
+#def link(users_list, n, users_id):
+#    m=[]
+#   for i in range(0, len(users_list[0]['tweets'])):
+#       m = [users_list[n]['tweets'][i]['user_mentions'][j] for j in range(0, len(users_list[n]['tweets'][i]['user_mentions'])) if (users_list[n]['tweets'][i]['user_mentions'][j] not in m and users_list[n]['tweets'][i]['user_mentions'][j] in users_id)]
+#    return m
+
+g = {nodes_list[i]['id']: links(nodes_list, i, users_count) for i in range(0, len(nodes_list))}
+print(len(g))
+print(g[nodes_list[600]['id']])
+print("ok")
